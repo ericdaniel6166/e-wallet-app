b24a32550b4374a9773c8351c198a14a8b78c03d
eric6166 <ericnguyen6166@gmail.com>
2022-10-31 19:32:26 +0700

    feature/0034-implement-grpc-api

--- grpcapi/server.go
+++ grpcapi/server.go
@@ -2,27 +2,19 @@ package grpcapi
 
 import (
 	"e-wallet-app/component"
-	"e-wallet-app/component/token"
 	"e-wallet-app/protogen"
-	"fmt"
 )
 
 type Server struct {
 	protogen.UnimplementedEWalletAppServer
-	tokenMaker token.Maker
-	appCtx     component.AppContext
+	appCtx component.AppContext
 }
 
 // NewServer creates a new gRPC server.
 func NewServer(appCtx component.AppContext) (*Server, error) {
-	tokenMaker, err := token.NewPasetoMaker(appCtx.TokenSymmetricKey())
-	if err != nil {
-		return nil, fmt.Errorf("cannot create token maker: %w", err)
-	}
 
 	server := &Server{
-		appCtx:     appCtx,
-		tokenMaker: tokenMaker,
+		appCtx: appCtx,
 	}
 
 	return server, nil
